2d8c818cff80356aa2a7ea03e60dcb1b80d5e714
Daniel-N98 <Danieldnee@hotmail.com>
2022-11-24 20:24:09 +0000

    Sorting & ordering implementation

--- models/items-model.js
+++ models/items-model.js
@@ -1,8 +1,18 @@
 import db from "../db/connection.js";
 
-const fetchItems = async (user_id) => {
+const fetchItems = async (user_id, query) => {
+  const validFields = ["cost", "quantity", "name"];
+  const { order, sort_by } = query;
+  if (
+    (sort_by && !validFields.includes(sort_by)) ||
+    (order && !["ASC", "DESC"].includes(order))
+  ) {
+    return Promise.reject({ code: 400, message: "Invalid query" });
+  }
   const results = await db.query(
-    `SELECT * FROM items WHERE user_id = ${user_id} ORDER BY item_id ASC`
+    `SELECT * FROM items WHERE user_id = ${user_id} ORDER BY ${
+      sort_by ? sort_by : "item_id"
+    } ${order ? order : "ASC"}`
   );
   return results.rows;
 };
