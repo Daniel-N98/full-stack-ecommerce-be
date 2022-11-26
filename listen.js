abd21b5ce359081029bdd9c22d7430b074342c9d
Daniel-N98 <Danieldnee@hotmail.com>
2022-11-26 16:21:01 +0000

    #

--- listen.js
+++ listen.js
@@ -2,6 +2,8 @@ import app from "./app.js";
 
 const { PORT = 9090 } = process.env;
 
+console.log(process.env.NODE_ENV);
+
 app.listen(PORT, (error) => {
   if (error) throw error;
   console.log(`Listening on port ${PORT}...`);
