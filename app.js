18467d6e2a1ce0ba0bbff40714dd16c31b83c6da
Daniel-N98 <Danieldnee@hotmail.com>
2022-11-27 12:09:38 +0000

    Added cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from "express";
 import apiRouter from "./routes/api-router.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors);
 app.use(express.json());
 app.use("/", apiRouter);
 
