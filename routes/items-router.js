79b6e42a3f23d985ebe08ecb46055ee4b17bdb27
Daniel-N98 <Danieldnee@hotmail.com>
2022-11-29 19:56:41 +0000

    Get items by category_id implementation

--- routes/items-router.js
+++ routes/items-router.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
-import { getItemByID, getItems } from "../controllers/items-controller.js";
+import {
+  getItemByID,
+  getItems,
+  getItemsByCategory,
+} from "../controllers/items-controller.js";
 const itemsRouter = Router();
 
+itemsRouter.get("/category/:category_id", getItemsByCategory);
 itemsRouter.get("/:user_id", getItems);
 itemsRouter.get("/:user_id/:item_id", getItemByID);
 
